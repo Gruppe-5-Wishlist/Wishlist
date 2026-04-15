30c86e42311b44321c65dfecd4953fed98d50a89
TOKJ <tokj0001@stud.ek.dk>
2026-04-15 12:17:46 +0200

    Wish og WishRepository virker måske :)

--- src/main/java/com/banditdev/wishlist/model/Wish.java
+++ src/main/java/com/banditdev/wishlist/model/Wish.java
@@ -1,21 +1,22 @@
 package com.banditdev.wishlist.model;
 
 public class Wish {
+
     private int wishId;
     private String wishName;
     private String wishDescription;
     private String wishLink;
     private double wishPrice;
+    private int wishlistId;
 
-
-    public Wish(int wishId, String wishName, String wishDescription, String wishLink, double wishPrice) {
+    public Wish(int wishId, String wishName, String wishDescription, String wishLink, double wishPrice, int wishlistId) {
         this.wishId = wishId;
         this.wishName = wishName;
         this.wishDescription = wishDescription;
         this.wishLink = wishLink;
         this.wishPrice = wishPrice;
+        this.wishlistId = wishlistId;
     }
-
 
     public int getWishId() {
         return wishId;
@@ -57,6 +58,14 @@ public class Wish {
         this.wishPrice = wishPrice;
     }
 
+    public int getWishlistId() {
+        return wishlistId;
+    }
+
+    public void setWishlistId(int wishlistId) {
+        this.wishlistId = wishlistId;
+    }
+
     @Override
     public String toString() {
         return "Wish{" +
@@ -65,6 +74,7 @@ public class Wish {
                 ", wishDescription='" + wishDescription + '\'' +
                 ", wishLink='" + wishLink + '\'' +
                 ", wishPrice=" + wishPrice +
+                ", wishlistId=" + wishlistId +
                 '}';
     }
 }
--- src/main/java/com/banditdev/wishlist/repository/WishRepository.java
+++ src/main/java/com/banditdev/wishlist/repository/WishRepository.java
@@ -1,7 +1,14 @@
 package com.banditdev.wishlist.repository;
 
+import com.banditdev.wishlist.model.Wish;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
 
 @Repository
 public class WishRepository {
@@ -12,13 +19,118 @@ public class WishRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    //TODO List<Object> findAll() {}
+    public List<Wish> findAll() {
 
-    //TODO public Object addObject(Object obj) {}
+        String sql = """
+                SELECT
+                    wish.wish_id,
+                    wish.wish_name,
+                    wish.wish_description,
+                    wish.wish_link,
+                    wish.wish_price
+                FROM wish
+                JOIN wishlist wl ON wish.wishlist_id = wl.wishlist_id
+                JOIN user u ON wl.user_id = u.user_id
+                ORDER BY 
+                    u.user_id,
+                    wl.wishlist_id,
+                    wish.wish_id;
+                """;
 
-    //TODO public void deleteObjectById(int idToDelete) {}
+        return jdbcTemplate.query(sql, (rs, rowNum) ->
+                new Wish(
+                        rs.getInt("wish_id"),
+                        rs.getString("wish_name"),
+                        rs.getString("wish_description"),
+                        rs.getString("wish_link"),
+                        rs.getDouble("wish_price"),
+                        rs.getInt("wishlist_id")
+                )
+        );
+    }
 
-    //TODO public Object findObjectById(int idToFind) {}
 
-    //TODO public void updateObject(Object obj) {}
+    public Wish addWish(Wish wish) {
+        String sql = """
+                INSERT INTO wishlist_db.wish (wish_name, wish_description, wish_link, wish_price, wishlist_id)
+                VALUES (?, ?, ?, ?, ?)
+                """;
+
+        KeyHolder kh = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, wish.getWishName());
+            ps.setString(2, wish.getWishDescription());
+            ps.setString(3, wish.getWishLink());
+            ps.setDouble(4, wish.getWishPrice());
+            ps.setInt(5, wish.getWishlistId());
+            return ps;
+        }, kh);
+
+        Number key = kh.getKey();
+        if (key == null) {
+            throw new IllegalStateException("Failed to get KeyHolder id.");
+        }
+
+
+        return new Wish(key.intValue(), wish.getWishName(), wish.getWishDescription(), wish.getWishLink(), wish.getWishPrice(), wish.getWishlistId());
+    }
+
+
+    public void deleteWishById(int idToDelete) {
+        String sql = """
+                DELETE FROM wish
+                WHERE wish_id = ?
+                """;
+        jdbcTemplate.update(sql, idToDelete);
+    }
+
+
+    public Wish findWishById(int idToFind) {
+        String sql = """
+                
+                        SELECT
+                    wish_id,
+                    wish_name,
+                    wish_description,
+                    wish_link,
+                    wish_price
+                FROM wish
+                WHERE wish_id = ?
+                """;
+
+        Wish result = jdbcTemplate.queryForObject(sql, (rs, rowNum) ->
+                        new Wish(
+                                rs.getInt("wish_id"),
+                                rs.getString("wish_name"),
+                                rs.getString("wish_description"),
+                                rs.getString("wish_link"),
+                                rs.getDouble("wish_price"),
+                                rs.getInt("wishlist_id")
+                        ),
+                idToFind
+        );
+        return
+                result;
+    }
+
+
+    public void updateWish(Wish wish) {
+        String sql = """
+                UPDATE wish
+                SET wish_name = ?, wish_description = ?, wish_link = ?, wish_price = ?
+                WHERE wish_id = ?
+                """;
+
+        jdbcTemplate.update(
+                sql,
+                wish.getWishName(),
+                wish.getWishDescription(),
+                wish.getWishLink(),
+                wish.getWishPrice(),
+                wish.getWishId()
+        );
+    }
+
 }
