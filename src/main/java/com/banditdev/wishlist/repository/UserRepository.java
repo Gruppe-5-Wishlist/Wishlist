b53acd19444c8b478d4eda9446125a23e4e66aa3
AlexArnold01 <alar1000@stud.ek.dk>
2026-04-15 12:33:27 +0200

    UserRepository.java -> completed addUser(), deleteUser() and updateUser() methods

--- src/main/java/com/banditdev/wishlist/repository/UserRepository.java
+++ src/main/java/com/banditdev/wishlist/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.banditdev.wishlist.repository;
 
 import com.banditdev.wishlist.model.User;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 @Repository
@@ -15,17 +19,71 @@ public class UserRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<User> findAll() {
-        return List.of();
+//    public List<User> findAll()
+
+    public User addUser (User user) {
+        String sql = """
+                INSERT INTO user (user_id, user_email, user_name, user_password) VALUES (?, ?, ?, ?)
+                """;
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, user.getUserId());
+            ps.setString(2, user.getUserEmail());
+            ps.setString(3, user.getUserName());
+            ps.setString(4, user.getUserPassword());
+            return ps;
+        }, keyHolder);
+
+        Number key = keyHolder.getKey();
+        if (key == null) {
+            throw new IllegalStateException("Failed to keyholder id");
+        }
+        return new User(key.intValue(), user.getUserEmail(), user.getUserName(), user.getUserPassword());
     }
 
+    public void deleteUser (int userID) {
+        String sql = """
+                DELETE FROM user
+                WHERE user_id = ?
+                """;
+        jdbcTemplate.update(sql, userID);
+    }
+
+//    public User findUserById (int id) {
+//        String sql = """
+//                SELECT
+//                    user_id,
+//                    user_email,
+//                    user_name,
+//                    user_password
+//                FROM user""";
+//    }
+
+    public void updateUser (User user) {
+        String sql = """
+                UPDATE user
+                SET user_email = ?, user_name = ?, user_password = ?
+                WHERE user_id = ?
+        """;
+
+        jdbcTemplate.update(
+                sql,
+                user.getUserEmail(),
+                user.getUserName(),
+                user.getUserPassword(),
+                user.getUserId()
+        );
+    }
     //TODO List<Object> findAll() {}
 
-    //TODO public Object addObject(Object obj) {}
+    //TODO public Object addObject(Object obj) {} -- COMPLETED
 
-    //TODO public void deleteObjectById(int idToDelete) {}
+    //TODO public void deleteObjectById(int idToDelete) {} -- COMPLETED
 
     //TODO public Object findObjectById(int idToFind) {}
 
-    //TODO public void updateObject(Object obj) {}
+    //TODO public void updateObject(Object obj) {} -- COMPLETED
 }
