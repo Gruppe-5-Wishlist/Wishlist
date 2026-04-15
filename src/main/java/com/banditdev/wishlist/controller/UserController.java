b9a3244f10cca461163fb17e3c1d40e9120ef9be
AlexArnold01 <alar1000@stud.ek.dk>
2026-04-15 13:34:31 +0200

    Startet på UserController klassen

--- src/main/java/com/banditdev/wishlist/controller/UserController.java
+++ src/main/java/com/banditdev/wishlist/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.banditdev.wishlist.controller;
 
 import com.banditdev.wishlist.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +14,10 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    private boolean isLoggedIn(HttpSession session) {
+        return session.getAttribute("user") != null;
+    }
+
 
 }
