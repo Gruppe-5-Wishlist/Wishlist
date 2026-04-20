152af3e7dba4820c9be0a948006f403d5fdb0a78
Frederick <frdu0001@stud@ek.dk>
2026-04-20 10:48:20 +0200

    added End point to WishlistController

--- src/main/java/com/banditdev/wishlist/controller/WishlistController.java
+++ src/main/java/com/banditdev/wishlist/controller/WishlistController.java
@@ -1,8 +1,12 @@
 package com.banditdev.wishlist.controller;
 
+import com.banditdev.wishlist.model.Wishlist;
 import com.banditdev.wishlist.service.WishlistService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("wishlist")
@@ -12,5 +16,12 @@ public class WishlistController {
 
     public WishlistController(WishlistService wishlistService) {
         this.wishlistService = wishlistService;
+    }
+
+
+    @PostMapping
+    public Wishlist createwishlist(@RequestBody Wishlist wishlist, @RequestParam int userId) {
+        return wishlistService.addWishlist(wishlist, userId);
+
     }
 }
