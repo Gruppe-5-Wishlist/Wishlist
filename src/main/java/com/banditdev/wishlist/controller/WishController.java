280bb4468b67ea5c1caedd5a152582fa8ded9354
AlexArnold01 <alar1000@stud.ek.dk>
2026-04-21 10:50:58 +0200

    added methods in WishController to fully support the html script

--- src/main/java/com/banditdev/wishlist/controller/WishController.java
+++ src/main/java/com/banditdev/wishlist/controller/WishController.java
@@ -1,9 +1,36 @@
 package com.banditdev.wishlist.controller;
 
+import com.banditdev.wishlist.model.Wish;
+import com.banditdev.wishlist.service.WishService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("wish")
 public class WishController {
+
+    private final WishService wishService;
+
+    public WishController(WishService wishService) {
+        this.wishService = wishService;
+    }
+
+    @PostMapping("/add/{wishlistId}")
+    public String addWish(@ModelAttribute Wish wish, @PathVariable int wishlistId) {
+        wishService.addWish(wish, wishlistId);
+        return "redirect:/wishlist" + wishlistId;
+    }
+
+    @PostMapping("/delete/{wishId}")
+    public String deleteWish(@PathVariable int wishId, @RequestParam int wishlistId) {
+        wishService.deleteWishById(wishId);
+        return "redirect:/wishlist/" + wishlistId;
+    }
+
+    @PostMapping("/update/{wishId}")
+    public String updateWish(@ModelAttribute Wish wish, @PathVariable int wishId) {
+        wish.setWishId(wishId);
+        wishService.updateWish(wish);
+        return "redirect:/wishlist/" + wish.getWishlistId();
+    }
 }
